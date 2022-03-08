647c06314dd6a37ee66882a02bc934e7193d9868
João Vitor <j.soares@aparkado.de>
2022-03-09 00:39:48 +0100

    hide debug banner

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -7,12 +7,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Error Handler',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomePage(),
+      home: HomePage(),
     );
   }
 }
